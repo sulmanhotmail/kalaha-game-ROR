e607a03e63b8c0ded0076424fb213e2d2c636fd0
Sulman Baig <sulman1990@hotmail.com>
2015-10-13 10:39:19 +0200

    changing the errors and info show

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,8 @@
 class HomeController < ApplicationController
   require 'game'
+  rescue_from Game::IllegalMoveError do |exception|
+    redirect_to root_path, :alert => exception.message
+  end
   $game = Game.new
 
   def main
@@ -12,14 +15,14 @@ class HomeController < ApplicationController
     my_input = params[:my_input].to_i
     $game.sow(my_input)
     respond_to do |format|
-      format.html { redirect_to root_path, notice: "redirected" }
+      format.html { redirect_to root_path, notice: "Move Applied." }
     end
   end
 
   def reset
     $game=Game.new
     respond_to do |format|
-      format.html { redirect_to root_path, notice: "redirected" }
+      format.html { redirect_to root_path, notice: "Game Reset Completed." }
     end
   end
 end
